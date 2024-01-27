1d223c5e0e860b88df4b1f2f71c0b4aa94f5463c
Alexandr Trifonov <ast89646421790@yandex.ru>
2024-01-27 15:05:33 +0300

    added files

--- src/test/java/tests/BaseTests.java
+++ src/test/java/tests/BaseTests.java
@@ -18,7 +18,7 @@ public class BaseTests {
         Configuration.browser = System.getProperty("browser","chrome");
         Configuration.browserVersion = System.getProperty("browserVersion","100.0");
         Configuration.browserSize = System.getProperty("resolution","1920x1080");
-        Configuration.baseUrl = System.getProperty("baseUrl", "https://www.cian.ru");
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://www.cian.ru/");
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = System.getProperty("wdSelenoid","https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
